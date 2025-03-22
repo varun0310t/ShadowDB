91e0fcba61c8a91356b593df1a454a7daa605779
varun0310t <chauhanvarun10th03@gmail.com>
2025-03-22 16:33:09 +0530

    non code changes

--- src/shadowdb/src/middleware.ts
+++ src/shadowdb/src/middleware.ts
@@ -19,6 +19,7 @@ export async function middleware(req: NextRequest) {
   return NextResponse.next();
 }
 
+
 export const config = {
   matcher: ["/Home/:path*", "/admin/:path*", "/api/protected/:path*"],
 };
