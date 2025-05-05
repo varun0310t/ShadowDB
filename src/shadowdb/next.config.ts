49b5c52ae85028910c8277576e93ef1cfd5bd5bd
varun0310t <chauhanvarun10th03@gmail.com>
2025-05-05 13:13:55 +0530

    added google auth

--- src/shadowdb/next.config.ts
+++ src/shadowdb/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com"], // Add your image domains here
+  },
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // Ignore ESLint errors during build
--- src/shadowdb/src/db/schemas/users.sql
+++ src/shadowdb/src/db/schemas/users.sql
@@ -20,7 +20,7 @@ END $$;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255), -- Nullable for OAuth users
   
   -- Authentication fields
